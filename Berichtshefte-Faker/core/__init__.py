e124aaea5484b5ce4521177865a6b714e75684a4
Askir4 <lionsin43@gmail.com>
2026-01-29 10:30:07 +0100

    Delete Berichtshefte-Faker directory

--- Berichtshefte-Faker/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# Core Module
-# Contains business logic for template handling, report generation, and export
-
-from .template_handler import TemplateHandler
-from .report_generator import ReportGenerator
-from .word_exporter import WordExporter
-
-__all__ = ['TemplateHandler', 'ReportGenerator', 'WordExporter']
--- Berichtshefte-Faker/gui/__init__.py
+++
@@ -1,8 +0,0 @@
-# GUI Module
-# Contains all graphical user interface components
-
-from .main_window import MainWindow
-from .template_manager import TemplateManagerFrame
-from .report_form import ReportFormFrame
-
-__all__ = ['MainWindow', 'TemplateManagerFrame', 'ReportFormFrame']
